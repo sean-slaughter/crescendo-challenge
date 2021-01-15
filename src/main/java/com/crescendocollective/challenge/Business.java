174c57d2d4b7ce58b77cf205e8465b78d1a0cd0c
sean-slaughter <sthompson2822@gmail.com>
2021-01-15 09:18:52 -0600

    create YelpService methods and Business model

--- src/main/java/com/crescendocollective/challenge/Business.java
+++ src/main/java/com/crescendocollective/challenge/Business.java
@@ -1,4 +1,18 @@
 package com.crescendocollective.challenge;
 
+import java.util.List;
+
+/**
+ * Business model class, contains list of reviews
+ */
 public class Business {
+    private List<Review> reviews;
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
 }
--- src/main/java/com/crescendocollective/challenge/YelpService.java
+++ src/main/java/com/crescendocollective/challenge/YelpService.java
@@ -1,7 +1,36 @@
 package com.crescendocollective.challenge;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+/**
+ * Service class to make calls to the Yelp Fusion API,
+ * Will return reviews for Tenuta's Italian Restaurant in Milwaukee
+ */
 @Service
+@PropertySource("classpath:yelp.properties")
 public class YelpService {
+    @Value("{$YELP_API_KEY")
+    private String key;
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    //create HttpEntity containing Yelp API authorization
+    private HttpEntity<String> getHttpEntity(){
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + key);
+        return new HttpEntity<String>(headers);
+    }
+    //send GET request to Yelp Fusion API and return Business object
+    public Business getReviews(){
+        String REVIEWS_QUERY = "https://api.yelp.com/v3/businesses/k0jk7sVdHDPqr-jnVJWlmA/reviews";
+        ResponseEntity<Business> response = restTemplate.exchange(
+                REVIEWS_QUERY, HttpMethod.GET, getHttpEntity(), Business.class);
+        return response.getBody();
+    }
 }
