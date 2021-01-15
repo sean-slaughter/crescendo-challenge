80507eb21799c96065facbb869a1edb4630676cf
sean-slaughter <sthompson2822@gmail.com>
2021-01-15 10:42:05 -0600

    add Google Vision API call to controller

--- src/main/java/com/crescendocollective/challenge/YelpController.java
+++ src/main/java/com/crescendocollective/challenge/YelpController.java
@@ -1,5 +1,8 @@
 package com.crescendocollective.challenge;
 
+import com.google.cloud.vision.v1.AnnotateImageResponse;
+import com.google.cloud.vision.v1.FaceAnnotation;
+import com.google.cloud.vision.v1.Feature;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gcp.vision.CloudVisionTemplate;
 import org.springframework.core.io.ResourceLoader;
@@ -20,9 +23,21 @@ public class YelpController {
 
     @GetMapping("/")
     public Business getReviews(){
+        //make yelp API call
         ResponseEntity<Business> b = yelpService.getReviewsResponse();
+        //check response
         if(b.getStatusCode() == HttpStatus.OK){
-           return b.getBody();
+           Business biz = b.getBody();
+           if(biz != null){
+               //iterate through reviews and run image url through Google Vision API
+               biz.getReviews().forEach(review -> {
+                   User user = review.getUser();
+                   AnnotateImageResponse resp = this.cloudVisionTemplate.analyzeImage(this.resourceLoader.getResource(user.getImage_url()), Feature.Type.FACE_DETECTION);
+                   FaceAnnotation faceAnnotation = resp.getFaceAnnotations(0);
+                   user.setFace(new Face(faceAnnotation));
+               });
+           }
+            return biz;
         }
         else{
             System.out.println("Something went wrong with your request");
