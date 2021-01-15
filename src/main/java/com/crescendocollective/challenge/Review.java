167e4e36a24cb4d87383e97fd5564e76a4d26f1c
sean-slaughter <sthompson2822@gmail.com>
2021-01-15 08:48:26 -0600

    add skeleton classes

---
+++ src/main/java/com/crescendocollective/challenge/Review.java
@@ -0,0 +1,4 @@
+package com.crescendocollective.challenge;
+
+public class Review {
+}
---
+++ src/main/java/com/crescendocollective/challenge/YelpService.java
@@ -0,0 +1,7 @@
+package com.crescendocollective.challenge;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class YelpService {
+}
