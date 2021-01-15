20bcc9d516ba60cc53257025dfb9d97f083af9f7
sean-slaughter <sthompson2822@gmail.com>
2021-01-15 09:54:08 -0600

    add YelpController

--- src/main/java/com/crescendocollective/challenge/YelpController.java
+++ src/main/java/com/crescendocollective/challenge/YelpController.java
@@ -1,8 +1,28 @@
 package com.crescendocollective.challenge;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class YelpController {
+
+    @Autowired
+    private YelpService yelpService;
+
+    @GetMapping("/")
+    public Business getReviews(){
+        ResponseEntity<Business> b = yelpService.getReviewsResponse();
+        if(b.getStatusCode() == HttpStatus.OK){
+           return b.getBody();
+        }
+        else{
+            System.out.println("Something went wrong with your request");
+            return null;
+        }
+    }
+
 
 }
