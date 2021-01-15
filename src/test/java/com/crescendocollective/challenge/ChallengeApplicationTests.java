79acab25af034909fcbaa974425d4642cfe74987
sean-slaughter <sthompson2822@gmail.com>
2021-01-15 08:51:41 -0600

    add context load tests

--- src/test/java/com/crescendocollective/challenge/ChallengeApplicationTests.java
+++ src/test/java/com/crescendocollective/challenge/ChallengeApplicationTests.java
@@ -1,13 +1,23 @@
 package com.crescendocollective.challenge;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class ChallengeApplicationTests {
 
+    @Autowired
+    private YelpController yelpController;
+
+    @Autowired
+    private YelpService yelpService;
+
     @Test
     void contextLoads() {
+        assertThat(yelpController).isNotNull();
+        assertThat(yelpService).isNotNull();
     }
 
 }
