4847bb41f0b16cc3112cb38268acfe685fcf15f6
sean-slaughter <sthompson2822@gmail.com>
2021-01-15 10:25:33 -0600

    add test for YelpController

--- src/test/java/com/crescendocollective/challenge/ChallengeApplicationTests.java
+++ src/test/java/com/crescendocollective/challenge/ChallengeApplicationTests.java
@@ -1,10 +1,15 @@
 package com.crescendocollective.challenge;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -13,18 +18,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class ChallengeApplicationTests {
 
     @Autowired
-    private YelpController yelpController;
-
-    @Autowired
     private YelpService yelpService;
+    @Autowired
+    private YelpController yelpController;
 
     @Test
     void contextLoads() {
-        assertThat(yelpController).isNotNull();
         assertThat(yelpService).isNotNull();
     }
 
-    //tests for Yelp Service class
+    //tests for YelpService class
     @Test
     void testYelpServiceGetHttpEntity(){
         ResponseEntity<Business> testResponse = yelpService.getReviewsResponse();
@@ -33,6 +36,15 @@ class ChallengeApplicationTests {
     @Test
     void testBusinessModel(){
         Business testBiz = yelpService.getBusiness(yelpService.getReviewsResponse());
+        assertThat(testBiz).isNotNull();
+        assertThat(testBiz.getReviews()).isNotNull();
+        assertThat(testBiz.getReviews().get(0)).isNotNull();
+    }
+
+    //test for YelpController
+    @Test
+    void testController(){
+        Business testBiz = yelpController.getReviews();
         assertThat(testBiz).isNotNull();
         assertThat(testBiz.getReviews()).isNotNull();
         assertThat(testBiz.getReviews().get(0)).isNotNull();
