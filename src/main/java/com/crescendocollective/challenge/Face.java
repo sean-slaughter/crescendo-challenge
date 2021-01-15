0415950bda49fd2e42307406b021a8c32559cb0d
sean-slaughter <sthompson2822@gmail.com>
2021-01-15 10:30:13 -0600

    add Face model class

--- src/main/java/com/crescendocollective/challenge/Face.java
+++ src/main/java/com/crescendocollective/challenge/Face.java
@@ -1,5 +1,37 @@
 package com.crescendocollective.challenge;
 
-public class Face {
+import com.google.cloud.vision.v1.FaceAnnotation;
 
+/**
+ * Face model class, returns likelihood of User
+ * emotions from Yelp Review User Image
+ */
+public class Face {
+    private final FaceAnnotation faceAnnotation;
+    private String joyLikelihood;
+    private String sorrowLikelihood;
+    private String angerLikelihood;
+    private String surpriseLikelihood;
+
+    //constructor takes in FaceAnnotation from API call
+    public Face(FaceAnnotation faceAnnotation){
+        this.faceAnnotation = faceAnnotation;
+    }
+
+    //getters and setters
+    public String getJoyLikelihood() {
+        return faceAnnotation.getJoyLikelihood().toString();
+    }
+
+    public String getSorrowLikelihood() {
+        return faceAnnotation.getSorrowLikelihood().toString();
+    }
+
+    public String getAngerLikelihood() {
+        return faceAnnotation.getAngerLikelihood().toString();
+    }
+
+    public String getSurpriseLikelihood() {
+        return faceAnnotation.getSurpriseLikelihood().toString();
+    }
 }
